26f00177a74432488cc4923a535118f202e51d92
JeffryGonzalez <jeff@hypertheory.com>
2024-08-20 11:53:02 -0400

    before pulldown

--- src/IssueTracker/IssueTrackerApi.Tests/BadCode.cs
+++ src/IssueTracker/IssueTrackerApi.Tests/BadCode.cs
@@ -7,11 +7,13 @@ public class BadCode
     public async Task CanGetStatus()
     {
 
-        var client = new HttpClient();
+        using var client = new HttpClient();
+
         client.BaseAddress = new Uri("http://localhost:1337");
 
         var response = await client.GetAsync("/status");
 
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
     }
 }
